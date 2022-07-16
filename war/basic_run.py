3d008c42ee337a376f41d6b6c452935f0f70ecae
Andrew Doyle <adoyle1215@gmail.com>
2022-07-15 19:25:51 -0700

    Refactoring and basic logging

--- war/basic_run.py
+++ war/basic_run.py
@@ -31,55 +31,78 @@ playerA = WarPlayer()
 playerB = WarPlayer()
 
 ## Split the deck between the playesr
-deckA, deckB = deck.split(num_split=2)
-playerA.deck.add_cardcollection(deckA)
-playerB.deck.add_cardcollection(deckB)
+splits = deck.split(num_split=2)
+playerA.deck.add_cardcollection(splits[0])
+playerB.deck.add_cardcollection(splits[1])
 
 # Avoid confusion later - we're done with these
-del deckA, deckB
+del splits
 
-# Basic stats
-num_turns = 0
+# Basic metadata
+num_turn = 1
+game_over = False
+debug = True
+
+if debug:
+    log_file = open("log.csv", "w")
+    header = "Num A Controlled,Num B Controlled," + \
+             "A Played,B Played,Reshuffled,B Reshuffled\n"
+    log_file.write(header)
 
 # Gameplay loop
-while playerA.get_controlled() not in [0,52]:
-    # TODO - This does *not* verify that both players have some cards they can
-    # flip. It simply shows that the total number of cards in one players active
-    # and discard piles is not 52 - it's perfectly possible for there to be
-    # cards still in both players' active areas.  I'm honestly not sure how to
-    # resolve this right now in terms of game rules, but we could perform a
-    # check on logic here for if either players deck and discard are both empty,
-    # perhaps comparing the active card (including "face down") cards in
-    # that case
-
+while not game_over:
     # Flip cards face-up
     playerA.flip()
     playerB.flip()
 
-    # Check to see if either player has won
-    if playerA.peek_active() > playerB.peek_active():
+    # Used in debugging (if applicable)
+    A_played = playerA.peek_active()
+    B_played = playerB.peek_active()
+    A_wins_round = (A_played > B_played)
+    B_wins_round = (B_played > A_played)
+    tie_round = not (A_wins_round or B_wins_round)
+
+    if A_wins_round:
         # Add both players' active areas to playerA's discard pile
         # and empty both players' active areas
         playerA.discard.add_cardcollection(playerA.empty("active"))
         playerA.discard.add_cardcollection(playerB.empty("active"))
 
-    elif playerB.peek_active() > playerA.peek_active():
+    if B_wins_round:
         # Add both players' active areas to playerB's discard pile
         # and empty both players' active areas
         playerB.discard.add_cardcollection(playerA.empty("active"))
         playerB.discard.add_cardcollection(playerB.empty("active"))
 
-    else:
+    if debug:
+        log_line = "{A_controlled},{B_controlled}," \
+        "{A_played},{B_played},{A_reshuffled},{B_reshuffled}\n".format(
+            A_controlled = playerA.get_controlled(),
+            B_controlled = playerB.get_controlled(),
+            A_played = A_played,
+            B_played = B_played,
+            A_reshuffled = ["", "X"][A_wins_round],
+            B_reshuffled = ["", "X"][B_wins_round]
+        )
+        log_file.write(log_line)
+
+    if tie_round:
         # The rank of both cards is equal - flip another card "face down" (it
         # identity will never be referenced, but can be moved between decks)
         playerA.flip()
         playerB.flip()
 
-    num_turns += 1
+    num_turn += 1
+    # TODO - Add other game over / victory conditions
+    game_over = playerA.get_controlled() == 52 or \
+                playerB.get_controlled() == 52
+
+if debug:
+    log_file.close()
 
 # Basic checks
 assert playerA.active.get_length() == 0
 assert playerB.active.get_length() == 0
 assert playerA.get_controlled() != playerB.get_controlled()
 assert (playerA.get_controlled() + playerB.get_controlled()) == 52
-print(num_turns)
+print(num_turn)
